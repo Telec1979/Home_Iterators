fa62fda5059b22948711073bbabce87c8c9ceeec
Denis Upadyshev <telec1979@yandex.ru>
2023-05-30 22:34:51 +0300

    Modify Task 1

--- main.py
+++ main.py
@@ -3,47 +3,56 @@ class FlatIterator:
     def __init__(self, list_of_list):
 
         # Задание №3 код для списка списков любого уровня вложенности
-
-        while True:
-            self.list = []
-            has_list = False
-            for element in list_of_list:
-                if type(element) is list:
-                    self.list.extend(element)
-                    has_list = True
-                else:
-                    self.list.append(element)
-            list_of_list = self.list
-            if not has_list:
-                break
-
-        # Задание 1 код для списка списков
         #
-        # self.list = []
-        # for lists in list_of_list:
-        #     self.list.extend(lists)
+        # while True:
+        #     self.list = []
+        #     has_list = False
+        #     for element in list_of_list:
+        #         if type(element) is list:
+        #             self.list.extend(element)
+        #             has_list = True
+        #         else:
+        #             self.list.append(element)
+        #     list_of_list = self.list
+        #     if not has_list:
+        #         break
 
+        # Задание 1 код для списка
+
+        self.list = list_of_list
         self.len = len(self.list)
 
     def __iter__(self):
-        self.count = 0
-
+        self.count1 = 0
+        self.count2 = 0
         return self
 
     def __next__(self):
-        self.count += 1
-        if self.count > self.len:
+        # реализация для задания 1
+        self.count2 += 1
+        if self.count2 > len(self.list[self.count1]):
+            self.count1 += 1
+            self.count2 = 1
+        if self.count1 == self.len:
             raise StopIteration
-        item = self.list[self.count-1]
+
+        item = self.list[self.count1][self.count2-1]
+        print(item)
         return item
 
+        # Реализация для задания 3
+        # self.count += 1
+        # if self.count > self.len:
+        #     raise StopIteration
+        # item = self.list[self.count-1]
+        # return item
 
 def test_1():
 
     list_of_lists_1 = [
         ['a', 'b', 'c'],
-        ['d', ['e', ['f', 'h']], False],
-        [1, [2, None]],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None],
         [3, 4, 6]
     ]
 
@@ -54,7 +63,7 @@ def test_1():
 
         assert flat_iterator_item == check_item
 
-    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, 3, 4, 6]
+    # assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, 3, 4, 6]
 
 
 if __name__ == '__main__':
